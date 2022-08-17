8481eb7e22cedd296ee943f185dcfb6bb5977918
pingmado <75196272+ping2109@users.noreply.github.com>
2022-08-17 18:01:42 +0700

    a50-common: More fixes to tree patching script

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,27 +2,31 @@ echo "I: - Fixing Stuff on A50 Device Trees"
 ### Start ###
 
 # Magisk update
+echo "I: - Fetching and applying the latest Magisk version"
 bash kernel/samsung/a50/usr/magisk/update_magisk.sh
 bash kernel/samsung/a50-common/usr/magisk/update_magisk.sh
 
+# Health fix
+echo "I: - Fixing Health hals"
+sudo rm -r hardware/interfaces/health/2.1/default/android.hardware.health@2.1-service.rc
+cp device/samsung/a50-common/patch/android.hardware.health@2.1-service.rc hardware/interfaces/health/2.1/default
+
 # Keystore patch
-echo "Applying Keystore patch...";
-cd system/security;
-git apply ../../device/samsung/a50-common/patch/Keystore.patch;
+echo "I: - Patching keystore"
+cd system/security
+git apply ../../device/samsung/a50-common/patch/Keystore.patch
 cd ../..
 
-# Light patch 
+# Light patch
+echo "I: - Patching light hals"
 cd frameworks/base
 wget https://raw.githubusercontent.com/sarthakroy2002/random-stuff/main/Patches/Fix-brightness-slider-curve-for-some-devices-a12l.patch 
 patch -p1 < *.patch
 cd ../..
 
-# Health fix
-
-sudo rm -r ./hardware/interfaces/health/2.1/default/android.hardware.health@2.1-service.rc
-cp ./device/samsung/a50-common/patch/android.hardware.health@2.1-service.rc ./hardware/interfaces/health/2.1/default/
 
 # BT calls fix
+echo "I: - Fixing BT hals"
 cd system/bt
-git apply device/samsung/a50-common/patch/BTCalls-On-Samsung.patch
+git apply ../../device/samsung/a50-common/patch/BTCalls-On-Samsung.patch
 cd ../..
